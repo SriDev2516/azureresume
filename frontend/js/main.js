ede46199c785f0899d14ee5e2ed95bc163c7651b
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-12-02 16:07:23 -0600

    Added production URL

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded',(event)=>{
     getVisitCount();
 })
 
-const functionApi = "http://localhost:7071/api/GetResumeCounter";
+const functionApiUrl ="https://counter20221202151314.azurewebsites.net/api/GetResumeCounter?code=zIXsK6Inpl2BBMrBrNKx-IGeZB3OVGz2lZNHYhWuMMCDAzFuz9WbXw==";
+const localfunctionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () =>{
     let count = 30;
-    fetch(functionApi).then(response=>{
+    fetch(functionApiUrl).then(response=>{
         return response.json()
     }).then(response =>{
         console.log("Function API was called");
